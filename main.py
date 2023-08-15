798eb197d325d18c44edb58e8469786af1933674
bru7682 <21569538-bru7682@users.noreply.replit.com>
2023-08-15 19:26:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+class car (vehicle):  
+   
+  def_init_(self,make,model,year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+ 
+def get_descriptive_name(self):
+    """Return a neatly formated describe name."""
+    long_name = f"{self.make} {self.model}" 
+    return long_name.title()
+
+class truck (vehcicle):
+
+   def setName(self, name):
+       self.truck_name = name
+
+   def displayOption(self):
+       print(f" The bed length is: {self.bed_length}")
+
+inputBed_length = input("Please enter your bed lenght:")
+input_car
