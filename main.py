d7d317ee6e6a9c31f7f1fbfc6219b1a8e4ce965b
Bru76k <141216892+Bru76k@users.noreply.github.com>
2023-08-15 21:45:17 +0000

    changes for class

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 class car:  
    
   def_init_(self,make,model,year):
-        self.make = make
-        self.model = model
-        self.year = year
+      self.make = make
+      self.model = model
+      self.year = year
 
  
 def get_descriptive_name(self):
@@ -13,11 +13,11 @@ def get_descriptive_name(self):
 
 class truck (car):
 
-   def setName(self, name):
-       self.truck_name = name
+   def setlength(self, length):
+       self.truck_length = length
 
    def displayOption(self):
-       print(f" The bed length is: {self.bed_length}")
+       print(f" The bed length is:{self.truck_length }")
 
 inputBed_length = input("Please enter your bed lenght:")
-input_car = input("Please inter what kind of  )
+input_car = input("Please inter what bed length:")
